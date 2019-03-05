f582ceb3dbada86a1ac5197ec44ce23be5ff8992
Paul <paul.ayling@bglgroup.co.uk>
2019-03-05 14:54:23 +0000

    Refactored Weather and added TestCase

---
+++ AirportCSharp.ClassLibraries/Weather.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace AirportCSharp.ClassLibraries
+{
+    public class Weather
+    {
+        public static int randomRoll()
+        {
+            Random rnd = new Random();
+            int roll = rnd.Next(1, 10);
+            return roll;
+        }
+
+        public static string assignWeather(int roll)
+        {
+            if (roll > 2) { return "Sunny"; }
+            return "Stormy";
+        }
+    }
+}
---
+++ AirportCSharp.UnitTest/WeatherTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AirportCSharp.ClassLibraries;
+using NUnit.Framework;
+
+namespace AirportCSharp.UnitTests
+{
+    [TestFixture]
+    public class WeatherTests
+    {
+        [TestCase(1, "Stormy")]
+        [TestCase(2, "Stormy")]
+        [TestCase(9, "Sunny")]
+        [TestCase(10, "Sunny")]
+        public void Returns_Sunny_Or_Stormy(int input, String output)
+        {
+            String weather = Weather.assignWeather(input);
+            Assert.That(weather, Is.EqualTo(output));
+
+        }
+    }
+}
