fefae06cb5ffd34f0246ca36df06eb6a42672a41
Paul <paul.ayling@bglgroup.co.uk>
2019-03-05 11:12:15 +0000

    * Airport.cs: Second Commit, added Class Libraries and Unit Tests
    * AirportCSharp.sln:
    * AirportCSharp.csproj:
    * UnitTests.cs:
    * AirportCSharp.UnitTests.csproj:
    * AirportCSharp.ClassLibraries.csproj:

---
+++ AirportCSharp.ClassLibraries/Airport.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AirportCSharp.ClassLibraries
+{
+    public class Airport
+    {
+    }
+}
---
+++ AirportCSharp.UnitTests/UnitTests.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using AirportCSharp.ClassLibraries;
+
+namespace AirportCSharp.UnitTests
+{
+    public class UnitTests
+    {
+    }
+}
