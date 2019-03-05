bb30ac454e5230c7985b4a09aeb76a7a1b1e51e0
Paul <paul.ayling@bglgroup.co.uk>
2019-03-05 11:24:48 +0000

    TestCheck
    Conflicts:
    	AirportCSharp.UnitTests/UnitTests.cs

--- AirportCSharp.UnitTests/UnitTests.cs
+++ AirportCSharp.UnitTests/UnitTests.cs
@@ -1,11 +1,24 @@
-﻿using System;
+using System;
+using NUnit.Framework;
 using AirportCSharp.ClassLibraries;
 
 namespace AirportCSharp.UnitTests
 {
+    [TestFixture]
     public class UnitTests
     {
+<<<<<<< HEAD
         //Just want to see if i'm able to push/ pull
     }
 }
+=======
+        [Test]
+        public void TestCheck()
+        {
+            Assert.AreEqual(3, 3);
+        }
+
+
+    }
+>>>>>>> 8dd9f4c... TestCheck
 }
